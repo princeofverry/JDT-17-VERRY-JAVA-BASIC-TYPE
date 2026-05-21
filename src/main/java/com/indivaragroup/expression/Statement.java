418e39b284d4fa5408219d3182ef3eaffbfb7a0f
princeofverry <verrykurniawan@students.undip.ac.id>
2026-05-21 13:50:18 +0700

    faet: statement

---
+++ src/main/java/com/indivaragroup/expression/Statement.java
@@ -0,0 +1,10 @@
+package com.indivaragroup.expression;
+
+public class Statement {
+    public static void statementShow() {
+        int a = 10; //assignment statement
+        a++; // increament statement
+        System.out.println(a); // method hasil statement
+        new String("Hello wok!"); // object creation statement
+    }
+}
